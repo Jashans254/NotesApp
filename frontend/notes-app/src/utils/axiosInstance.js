2d99eb8ec0af2e138d8b6bf9a328f44b3b5f3a87
jashanpreet singh <jashanprts254@gmail.com>
2024-08-01 21:04:04 +0530

    updated url

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL ,
+    baseURL: "https://notesapp-wzor.onrender.com" ,
     timeout: 10000 ,
     headers:{
         'Content-Type': 'application/json'
